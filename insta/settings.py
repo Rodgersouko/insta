a5565b48cb921086b58104a8d181e96aaf6eba1d
Rodgersouko <gabrielaouko@gmail.com>
2020-11-22 23:16:56 +0300

    create signup & login templates

--- insta/settings.py
+++ insta/settings.py
@@ -43,8 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'insta',
+    #'crispy_forms',
+
 
 ]
+#CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
